930ea8b4831f26bc75de5bf9b799a2128423b942
Sergio Valiente <sevalien@ucm.es>
2025-09-30 15:38:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function devuelveTextoDeAlerta() {
-  return "uooooo! Vaya alerta";
-}
 
-function desaparece(nombre) {
-	var button = document.getElementById(nombre);
-  button.style.visibility='hidden';
-}
