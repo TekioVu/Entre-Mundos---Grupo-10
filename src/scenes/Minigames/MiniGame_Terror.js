bd55c24ad4d1bd6e8094e31296848b0ffeba9670
Diegogog25 <diegog25@ucm.es>
2025-12-04 16:50:34 +0100

    Minijuego de Terror acabado

--- src/scenes/Minigames/MiniGame_Terror.js
+++ src/scenes/Minigames/MiniGame_Terror.js
@@ -16,7 +16,7 @@ export default class MiniGame_Terror extends Phaser.Scene {
         const cy = height / 2;
         //Bucle
         this.finished = false;
-        this.timeLeft = 1.5;
+        this.timeLeft = 1.1;
         //----------------------------------------
         // Fondo 
         //----------------------------------------
@@ -30,13 +30,20 @@ export default class MiniGame_Terror extends Phaser.Scene {
             .setOrigin(0.5)
             .setStrokeStyle(strokeWidth, 0xffffff);
 
-        //----------------------------------------
-        // Zona Perfecta
-        //----------------------------------------
+        
         this.perfectWidth = barWidth * 0.05;
+        this.normalWidth = barWidth * 0.1;
+                
+        this.totalWidth = this.normalWidth/2 + this.perfectWidth/2 + 10;
+
+        this.randomL = cx - barWidth/2  + this.totalWidth;
+        this.randomR = cx + barWidth/2  - this.totalWidth;
+        this.random = Phaser.Math.Between(this.randomL, this.randomR);
+   
+        // Zona Perfecta
 
         this.perfectZone = this.add.rectangle(
-            cx,
+            this.random,
             cy,
             this.perfectWidth,
             barHeight - strokeWidth,
@@ -44,10 +51,9 @@ export default class MiniGame_Terror extends Phaser.Scene {
             ).setOrigin(0.5);
 
         //Zonas normales
-        this.normalWidth = barWidth * 0.05;
-
+    
         this.rightnormalZone = this.add.rectangle(
-            cx + this.perfectWidth,
+            this.random + this.perfectWidth * 1.5,
             cy,
             this.normalWidth,
             barHeight - strokeWidth,
@@ -55,15 +61,15 @@ export default class MiniGame_Terror extends Phaser.Scene {
             ).setOrigin(0.5);
 
         this.leftnormalZone= this.add.rectangle(
-            cx - this.perfectWidth,
+            this.random - this.perfectWidth * 1.5,
             cy,
             this.normalWidth,
             barHeight - strokeWidth,
-            0xffff00
+             0xffff00
             ).setOrigin(0.5);
 
         // ----- CURSOR -----
-        this.cursorWidth = 10;
+        this.cursorWidth = 5;
         this.cursor = this.add.rectangle(
             cx - barWidth / 2 + this.cursorWidth/2,  
             cy,
@@ -73,8 +79,10 @@ export default class MiniGame_Terror extends Phaser.Scene {
         ).setOrigin(0.5);
         
         //Movimiento del cursor
-        this.cursorSpeed = 400;
+        this.cursorSpeed = 600;
         this.cursorDir = 1
+        this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+        
 
         this.leftLimit = cx - barWidth/2 + this.cursorWidth/2;
         this.rightLimit = cx + barWidth/2 - this.cursorWidth/2;
@@ -86,17 +94,61 @@ export default class MiniGame_Terror extends Phaser.Scene {
         if(this.finished)return; 
         this.cursor.x +=  this.cursorDir * this.cursorSpeed * this.game.loop.delta / 1000;
         if(this.cursor.x < this.leftLimit)this.cursorDir = 1;
-        if(this.cursor.x > this.rightLimit)this.cursorDir = -1;
-            
+        if(this.cursor.x > this.rightLimit)this.cursorDir = -1;          
         }
 
+
     update(time,delta){
+        if (Phaser.Input.Keyboard.JustDown(this.spaceKey)){
+            this.checkResult();
+            return;
+        }
+
+
         if(this.finished)return;
         this.timeLeft -= delta / 1000;
+
         if(this.timeLeft <= 0){
-            this.finished = true;
+            this.finish("fail");
             return
         }
+    
         this.cursorMovement();
+    }
+
+    checkResult(){
+        const x = this.cursor.x;
+        let result;
+        // Perfecta
+        const rightPerfect = this.perfectZone.x + this.perfectWidth/2;
+        const leftPerfect = this.perfectZone.x - this.perfectWidth/2;
+
+        // normal Derecha
+        const normalRightRight = this.rightnormalZone.x + this.normalWidth/2;
+        const normalRightLeft = this.rightnormalZone.x - this.normalWidth/2;
+        
+        // normal Izquierda
+        const normalLeftRight = this.leftnormalZone.x + this.normalWidth/2;
+        const normalLeftLeft = this.leftnormalZone.x - this.normalWidth/2;
+
+        if (x>=leftPerfect && x<= rightPerfect) result = "perfect";
+        else if ((x>=normalLeftLeft && x<= normalLeftRight) 
+                ||(x>= normalRightLeft && x<= normalRightRight)) result = "normal";
+        else result = "fail";
+
+        this.finish(result);
+
+    }
+
+
+
+    finish(result) {
+        if (this.finished) return;
+        this.finished = true;
+
+        this.time.delayedCall(350, () => {
+            this.parent.minigameResult(result);
+            this.scene.stop();
+        });
     }
 }
